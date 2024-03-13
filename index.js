2bf967b8cb2599cbd052b5a2ae90df80a1431d27
Mathieu Bour <mathieu@bour.tech>
2024-03-13 09:41:03 +0100

    feat: init workspace

---
+++ index.js
@@ -0,0 +1,3 @@
+// Write a simple HTTP application that listens on port 3000 with the following routes:
+// GET /users: returns a list of users, loaded from the file users.json
+// POST /users: accepts a JSON body with a user and adds it to the list of users
