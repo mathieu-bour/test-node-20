bf7c4f65bbd9f8c810409f2c8486270bc46480ca
Mathieu Bour <mathieu@bour.tech>
2024-03-13 12:44:47 +0100

    feat: start with an express template

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Start here :)
+import express from "express";
+
+const app = express();
+app.use(express.json()); // allow to use json data in request body
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
